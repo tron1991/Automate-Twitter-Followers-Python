f4abff4e9e0245e2fa6ff724dc5d7247eefc70a2
Nicholas Ivanecky <ivanecky844@hotmail.com>
2016-01-26 23:47:47 -0500

    Minor change in files

---
+++ sample_twitter_codes.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+'''
+Auto-Follow 40 followers of the username below
+'''
+from twitter_follow_bot import auto_follow
+Auto_Follow("newyork", count=40)
+
+from twitter_follow_bot import auto_follow
+Auto_Follow("sanfrancisco", count=40)
+
+'''
+Auto-Follow the followers of the user below
+'''
+
+from twitter_follow_bot import auto_follow_followers_for_user
+auto_follow_followers_for_user("iOSDevHelper", count=40)
+
+'''
+Unfollow any Non-Followers below
+'''
+
+'''
+from twitter_follow_bot import auto_unfollow_nonfollowers
+auto_unfollow_nonfollowers()
+'''
+
+
+
+
+
+
+
